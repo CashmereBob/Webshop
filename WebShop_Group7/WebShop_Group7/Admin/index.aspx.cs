5d717ab6dd1a240e104e2543c74a95971c1be3d0
lundgren84 <lundgren84@hotmail.se>
2016-09-19 14:08:51 +0200

    asd

--- WebShop_Group7/WebShop_Group7/Admin/index.aspx.cs
+++ WebShop_Group7/WebShop_Group7/Admin/index.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace WebShop_Group7.Admin
 {
-    public partial class Admin_LoginPage : System.Web.UI.Page
+    public partial class index : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
