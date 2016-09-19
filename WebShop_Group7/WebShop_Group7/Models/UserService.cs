162b6f3c38785149b7c22a7376e90485c96cc997
CashmereBob <weidmar@gmail.com>
2016-09-19 20:51:16 +0200

    Hashing kommentarer

--- WebShop_Group7/WebShop_Group7/Models/UserService.cs
+++ WebShop_Group7/WebShop_Group7/Models/UserService.cs
@@ -7,22 +7,21 @@ namespace WebShop_Group7.Models
 {
     public class UserService
     {
-        public String CreateSalt(int size)
+        public String CreateSalt(int size) //Metod för att skapa salt, tar en inparameter som bestämmer längden på saltet.
         {
-            var rng = new System.Security.Cryptography.RNGCryptoServiceProvider();
-            var buff = new byte[size];
-            rng.GetBytes(buff);
-            return Convert.ToBase64String(buff);
+            var rng = new System.Security.Cryptography.RNGCryptoServiceProvider(); //Skapar upp en ny "Random generator" från security namespase.
+            var buff = new byte[size]; //Skapar upp en array med längden från inparametern.
+            rng.GetBytes(buff); //Random genererar bytes i arrayen
+            return Convert.ToBase64String(buff); //Konverterar och returnerar det nu färdiga saltet.
         }
 
-        public String GenerateSHA256Hash(String input, String salt)
+        public String GenerateSHA256Hash(String input, String salt) //Metod för att hasha lösenordet tillsammans med ett salt
         {
-            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(input + salt);
-            System.Security.Cryptography.SHA256Managed sha256hashstring =
-                new System.Security.Cryptography.SHA256Managed();
-            byte[] hash = sha256hashstring.ComputeHash(bytes);
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(input + salt); //Kodar om input tillsammans med saltet och lägger i en byte array
+            var sha256hashstring = new System.Security.Cryptography.SHA256Managed(); //Skapar upp en SHA256 krypterare från namespacet security
+            byte[] hash = sha256hashstring.ComputeHash(bytes); //Skickar in vår byte variabel till vår SHA256 krypterare och tilldear hashen till en variabel
 
-            return hash.ToString();
+            return Convert.ToBase64String(hash); //Konverterar hashen till en string och returnerar.
         }
     }
 }
