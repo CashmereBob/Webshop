2478907381756d5080f2a63b521966a684e97eeb
CashmereBob <weidmar@gmail.com>
2016-09-19 20:39:53 +0200

    Fixat en hash och salt

---
+++ WebShop_Group7/WebShop_Group7/Models/UserService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebShop_Group7.Models
+{
+    public class UserService
+    {
+        public String CreateSalt(int size)
+        {
+            var rng = new System.Security.Cryptography.RNGCryptoServiceProvider();
+            var buff = new byte[size];
+            rng.GetBytes(buff);
+            return Convert.ToBase64String(buff);
+        }
+
+        public String GenerateSHA256Hash(String input, String salt)
+        {
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(input + salt);
+            System.Security.Cryptography.SHA256Managed sha256hashstring =
+                new System.Security.Cryptography.SHA256Managed();
+            byte[] hash = sha256hashstring.ComputeHash(bytes);
+
+            return hash.ToString();
+        }
+    }
+}
