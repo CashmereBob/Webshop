a7b3accd9850bb6f0946d3517aa04f81ac0fccba
CashmereBob <weidmar@gmail.com>
2016-09-20 12:48:27 +0200

    dbconnection

--- WebShop_Group7/WebShop_Group7/Models/DBConnection.cs
+++ WebShop_Group7/WebShop_Group7/Models/DBConnection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,13 +8,65 @@ namespace WebShop_Group7.Models
 {
     public class DBConnection
     {
-        private string _connectionString;
+        private SqlConnection _connection = new SqlConnection();
+        private string _connectionString = @"Data Source=Local;User ID=Magnus; Password=12345678;Integrated Security = false";
 
-        public DBConnect(string conecctionstring)
+        public DBConnection()
         {
+            try
+            {
+                OpenConnection();
 
+                var admin = new List<int>();
 
+                string sql = "Select ID From tbl_Users";
+                SqlCommand myCommand = new SqlCommand(sql, _connection);
+
+                using (SqlDataReader myDataReader = myCommand.ExecuteReader())
+                {
+                    while (myDataReader.Read())
+                    {
+                        admin.Add(int.Parse(myDataReader["ID"].ToString()));
+                    }
+                }
+
+                if (admin.Count() < 1)
+                {
+                    AddAdmin();
+                }
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                CloseConnection();
+            }
         }
 
+        public void OpenConnection()
+        {
+            _connection.ConnectionString = _connectionString;
+            _connection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            _connection.Close();
+        }
+
+        public void AddAdmin()
+        {
+            var user = new UserService();
+
+            var salt = user.CreateSalt(10);
+            var password = user.GenerateSHA256Hash("password", salt);
+
+            string sql = $"Insert Into tbl_Users (Firstname, Lastname, Adress, Postalcode, City, Email, Telephone, Mobilephone, Password, Salt, Pricegroup, Company, Admin) " + 
+                $"Values('Admin', 'Admin', 'Admin', 'Admin', 'Admin', 'Admin', 'Admin', 'Admin', '{password}', '{salt}', '1', 'Admin', '1' )";
+            SqlCommand myCommand = new SqlCommand(sql, _connection);
+        }
     }
 }
